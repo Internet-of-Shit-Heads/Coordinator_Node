53aa1d53a8821f2b0459f92732f493f4a0684a30
Thomas <thomas.weber@elektrotauchner.at>
2018-02-16 19:06:14 +0100

    working wifi+rf

--- Coordinator_Node/Coordinator_Node.ino
+++ Coordinator_Node/Coordinator_Node.ino
@@ -1,3 +1,7 @@
+#include <sensordata.pb.h>
+#include <pb_decode.h>
+#include <pb_encode.h>
+
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <WiFiClientSecure.h>
@@ -14,36 +18,62 @@
 
 const uint64_t pipes[1] = { 0xF0F0F0F0E1LL };
 
-WiFiClientSecure wifiClient;
-PubSubClient client(wifiClient);
+static WiFiClientSecure wifiClient;
+static PubSubClient client(wifiClient);
 
-static struct rflib_msg_t ackmsg = {
-  9, "BAM OIDA"
-};
+static struct rflib_msg_t ackmsg;
+static at_ac_tuwien_iot1718_N2C msg_to_recv;
+
+/*static struct {
+  uint64_t address = 0xF0F0F0F0E1LL;
+  unsigned int baud_rate = 57600;
+  uint16_t cepin = 9;
+  uint16_t cspin = 10;
+  uint8_t channel = 0;
+  uint8_t delay = 15;
+  uint8_t retransmits = 15;
+} coordinator_config;
+*/
+static bool pre_send(rflib_msg_t *msg, uint32_t timestamp)
+{
+  static at_ac_tuwien_iot1718_C2N msg_to_send = at_ac_tuwien_iot1718_C2N_init_zero;
+  msg_to_send.timestamp = timestamp;
+
+  pb_ostream_t stream = pb_ostream_from_buffer(msg->data, RFLIB_MAX_MSGSIZE);
+  bool enc_res = pb_encode(&stream, at_ac_tuwien_iot1718_C2N_fields, &msg_to_send);
+  msg->size = enc_res ? stream.bytes_written : 0;
+  Serial.println(msg->size);
+  return enc_res;
+}
+
+static bool post_recv(rflib_msg_t *msg, at_ac_tuwien_iot1718_N2C *msg_to_recv)
+{
+  pb_istream_t stream = pb_istream_from_buffer(msg->data, msg->size);
+  return pb_decode(&stream, at_ac_tuwien_iot1718_N2C_fields, msg_to_recv);
+}
 
 void setup() {
   Serial.begin(115200);
-  setup_wifi();
+  setup_certificates();
+  wifi_connect();
   client.setServer(mqtt_server, 1883);
-  int valuerflib = 0;
-  Serial.begin(115200);
-
   Serial.print("Begin init\n\r");
   // Give some time 
   delay(100);
-  if (valuerflib = rflib_coordinator_init(0, 15, 0, pipes, 1, 15, 15) < 0) {
+  if (rflib_coordinator_init(0, 15, 0, pipes, 1, 15, 15) < 0) {
     Serial.print("Init failed :(\n\r");
-    Serial.print(valuerflib);
     abort();
   }else{
-    Serial.print("Init success :(\n\r");  
+    Serial.print("Init success :(\n\r"); 
+    //rflib_coordinator_set_reply(0, &ackmsg);       
   }
 }
 
 static bool ackmsg_loaded = false;
 static void toggle_ackmsg(void)
 {
-  Serial.print("toggling\n\r");
+  pre_send(&ackmsg, 5);//timestamp is static 5  
+  Serial.print("Sending\n\r");
   if (ackmsg_loaded) {
     rflib_coordinator_clear_reply();
     ackmsg_loaded = false;
@@ -53,7 +83,7 @@ static void toggle_ackmsg(void)
   }
 }
 
-void setup_wifi() {
+static void setup_certificates() {
   
   delay(10);
   //load ca cert
@@ -75,7 +105,7 @@ void setup_wifi() {
     return;
   }
   
-  File ca = SPIFFS.open("/ca.pem.der", "r"); 
+  File ca = SPIFFS.open("/ca.der", "r"); 
   if (!ca) {
     Serial.println("Failed to open CA file");
   }
@@ -86,7 +116,9 @@ void setup_wifi() {
     else
     Serial.println("CA not loaded");
   }
-  
+}
+
+static void wifi_connect(){  
   // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
@@ -100,10 +132,10 @@ void setup_wifi() {
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP()); 
+  Serial.println(WiFi.localIP());   
 }
 
-void reconnect() {
+static void reconnect() {
   while (!wifiClient.connected()) {
     Serial.print("Attempting MQTT connection...");
     if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
@@ -118,9 +150,9 @@ void reconnect() {
   }
 }
 
-long lastMsg = 0;
-float temp = 0.0;
-float hum = 0.0;
+static long lastMsg = 0;
+static float temp = 0.0;
+static float hum = 0.0;
 static rflib_msg_t msg;
 static uint8_t runcnt = 0;
 
@@ -129,38 +161,41 @@ void loop() {
     reconnect();
   }
   client.loop();
-
-  long now = millis();
-  if (now - lastMsg > 1000) {
-    lastMsg = now;
-
-    float newTemp = 80;//= hdc.readTemperature();
-    float newHum = 70;//hdc.readHumidity();
-
-    temp = newTemp;
-    Serial.print("New temperature:");
-    Serial.println(String(temp).c_str());
-    client.publish(temperature_topic, String(temp).c_str(), false);
-
+ 
+  float newHum = 70;//hdc.readHumidity();
  /*   hum = newHum;
     Serial.print("New humidity:");
     Serial.println(String(hum).c_str());
     client.publish(humidity_topic, String(hum).c_str(), false);
 */
-      if (runcnt % 5 == 0) {
+ /*     if (runcnt % 5 == 0) {
         toggle_ackmsg();
-      }
+      }*/
       runcnt++;
+      pre_send(&ackmsg, runcnt);//timestamp is static 5  
+      rflib_coordinator_set_reply(0, &ackmsg);      
 
       if (rflib_coordinator_available() == 0) {
         rflib_coordinator_read(&msg);
-        Serial.print("Message-Content");
-        Serial.print((char *)msg.data);
+        
+        if (post_recv(&msg, &msg_to_recv)){
+            Serial.println("Tutto bene!");  
+            Serial.print("Room number: ");
+            Serial.println(msg_to_recv.roomNo);
+            Serial.print("Type: ");
+            Serial.println(msg_to_recv.type);
+            Serial.print("Timestamp: ");
+            Serial.println(msg_to_recv.timestamp);
+            Serial.print("Sensordata: ");
+            Serial.println(msg_to_recv.data);
+          }else{
+            Serial.println("Decoding failed! :(");  
+        }
         Serial.print("\n\r"); 
         //publish humidity value
         newHum = (float)* msg.data;
         Serial.println(String(newHum).c_str());
         client.publish(humidity_topic, String(newHum).c_str(), false);
       }
-    }
+  delay(50);
 }
